4717df61bffaba91521c51e353c54ce8d1d605d3
Ben <ben.liu@kmart.com.au>
2021-10-29 10:23:42 +1100

    create minimal reproduction code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  const signIn = () => {
+    loginWithRedirect({
+      redirectUri: `http://localhost:3001`,
+    });
+  };
+
+  const logOutHandler = () => {
+    logout({
+      returnTo: `http://localhost:3001`,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={signIn}>Login</button>
+      <button onClick={logOutHandler}>Logout</button>
+      {isAuthenticated ? "Logged in" : "not logged in"}
     </div>
   );
 }
